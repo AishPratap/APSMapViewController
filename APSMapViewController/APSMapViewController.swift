e725c05d12f33003dcae78b3419c16c309d4d416
Aishwarya Pratap Singh <pratapsingh.aishwarya@hotmail.com>
2017-05-12 11:47:30 -0700

    first commit

---
+++ APSMapViewController/APSMapViewController.swift
@@ -0,0 +1,18 @@
+//
+//  APSMapViewController.swift
+//  APSMapViewController
+//
+//  Created by Aishwarya Pratap Singh on 5/12/17.
+//  Copyright © 2017 Aishwarya Pratap Singh. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MapKit
+
+class APSMapViewController: UIViewController {
+    
+    @IBOutlet weak var mapview: MKMapView!
+    
+    
+}
