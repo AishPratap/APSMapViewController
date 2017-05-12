0a74173dc8859808c6a3421d9a02d811ddd6bf08
Aishwarya Pratap Singh <pratapsingh.aishwarya@hotmail.com>
2017-05-12 13:58:35 -0700

    added annotation support

---
+++ APSMapViewController/APSLocation.swift
@@ -0,0 +1,9 @@
+//
+//  APSLocation.swift
+//  APSMapViewController
+//
+//  Created by Aishwarya Pratap Singh on 5/12/17.
+//  Copyright © 2017 Aishwarya Pratap Singh. All rights reserved.
+//
+
+import Foundation
